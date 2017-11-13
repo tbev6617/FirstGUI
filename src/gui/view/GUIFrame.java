4834aaa648392e600932617edf8f6b40ea43e421
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2017-11-13 08:43:06 -0700

    the button is in spring layout

--- src/gui/view/GUIFrame.java
+++ src/gui/view/GUIFrame.java
@@ -6,16 +6,20 @@ import javax.swing.JFrame;
 public class GUIFrame extends JFrame
 {
 	private GUIAppController appController;
+	private GUIPanel firstPanel;
 	public GUIFrame(GUIAppController appController)
 	{
 		super();
 		this.appController = appController;
+		this.firstPanel = new GUIPanel(appController);
 		setupFrame();
 	}
 	private void setupFrame() 
 	{
+		this.setContentPane(firstPanel);
 		this.setTitle("The window title");
 		this.setSize(500, 500);
+		//this.setResizable(false);
 		this.setVisible(true);
 	}
 }
--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -3,16 +3,38 @@ package gui.view;
 import gui.controller.GUIAppController;
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import javax.swing.SpringLayout;
 import java.awt.Color;
 
 public class GUIPanel extends JPanel
 {
 	private GUIAppController appController;
 	private JButton firstButton;
+	private SpringLayout baseLayout;
 	public GUIPanel(GUIAppController appController)
 	{
 		super();
 		this.appController = appController;
 		firstButton = new JButton("WOW a button");
+		baseLayout = new SpringLayout();
+		setupPanel();
+	}
+	/**
+	 * Used to install all components to the subclass of JPanel
+	 * This installs
+	 */
+	private void setupPanel()
+	{
+		this.setLayout(baseLayout);
+		this.add(firstButton);
+		//calls add method of the super class to add firstButton
+	}
+	private void setupLayout()
+	{
+		
+	}
+	private void setupListeners()
+	{
+		
 	}
 }
